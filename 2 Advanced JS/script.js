78303a1fea3d567a7f834756eb5d6fd922d5e76b
bayyaramharsha <bayyaramharsha@gmail.com>
2023-08-03 18:29:35 +0530

    second vid completed

---
+++ 2 Advanced JS/script.js
@@ -0,0 +1,53 @@
+/*
+
+function abcd() {
+    for (var i = 1; i < 12; i++) {
+        console.log(i);
+    }
+    console.log(i)
+}
+
+abcd();
+
+
+
+function abcd() {
+    for (var i = 1; i < 12; i++) {
+        console.log(i);
+    }
+    console.log(i)
+}
+
+abcd();
+
+var a = 12;
+
+let b = 20;
+
+var a = [1, 2, 3, 4, 5];
+var b = [...a];
+
+b.pop();
+
+
+var a = { name: "harsha" };
+
+var b = {...a };
+
+
+var a = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
+
+a.forEach(function(val) {
+    console.log(val + 2);
+});
+
+
+var a = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
+
+a.forEach(function(val) {
+    console.log(val);
+});
+
+
+
+*/
